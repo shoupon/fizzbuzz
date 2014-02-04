a9d5718b1f1841205cc79f56d68d7af785355555
shouponlin <shoupon.lin@gmail.com>
2014-02-04 11:17:39 -0500

    script printing numbers [1,100] and names

---
+++ shoupon_lin_sl3357.rb
@@ -0,0 +1,14 @@
+for x in 1..100
+  if x % 3 == 0 && x % 5 == 0
+    print "Shou-pon Lin"
+  elsif x % 3 == 0
+    print "Shou-pon"
+  elsif x % 5 == 0
+    print "Lin"
+  elsif 
+    print x
+  end
+  print " " 
+end
+print "\n"
+
